8b13c35e0db5857db1ecc992f029f8916751fe25
natali <natashakubakh76@gmail.com>
2021-01-10 21:09:05 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mycriptoapp/api/ApiService.kt
@@ -0,0 +1,39 @@
+package com.example.mycriptoapp.api
+
+import com.example.mycriptoapp.pojo.CoinInfoListOfData
+import com.example.mycriptoapp.pojo.CoinPriceInfoRawData
+import io.reactivex.rxjava3.core.Single
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiService {
+
+    @GET("top/totalvolfull")
+    fun getTopCoinsInfo(
+        @Query(QUERY_PARAM_API_KEY) apiKey: String = "8bc9be25cd36e4444256f52a3993ec00ed6fa1e470ec07c0b126c1f6dfc89648",
+        @Query(QUERY_PARAM_LIMIT) limit: Int = 10,
+        @Query(QUERY_PARAM_TO_SYMBOL) tSym: String = CURRENCY,
+    ): Single<CoinInfoListOfData>
+
+    @GET("pricemultifull")
+    fun getFullPriceList(
+        @Query(QUERY_PARAM_API_KEY) apiKey: String = "8bc9be25cd36e4444256f52a3993ec00ed6fa1e470ec07c0b126c1f6dfc89648",
+        @Query(QUERY_PARAM_FROM_SYMBOLS) fSyms: String,
+        @Query(QUERY_PARAM_TO_SYMBOLS) tSyms: String= CURRENCY
+
+
+
+        ):Single<CoinPriceInfoRawData>
+
+    companion object {
+        private const val QUERY_PARAM_API_KEY = "api_key"
+        private const val QUERY_PARAM_LIMIT = "limit"
+        private const val QUERY_PARAM_TO_SYMBOL = "tsym"
+        private const val QUERY_PARAM_TO_SYMBOLS = "tsyms"
+        private const val QUERY_PARAM_FROM_SYMBOLS = "fsyms"
+
+
+        private const val CURRENCY = "USD"
+
+    }
+}
